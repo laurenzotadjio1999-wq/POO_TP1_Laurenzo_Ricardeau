04af78184c78810553c6f9909a2f3a44d0d70ea6
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2025-12-13 23:40:39 -0500

    mise à jour

--- Classe Bibliotheque.py
+++ Classe Bibliotheque.py
@@ -2,5 +2,8 @@
 class Bibliotheque:
     def __init__(self):
         self.liste_documents = {}
-        self.liste_adherents = {} # CORRECTION: Utiliser le pluriel et la minuscule pour la cohérence
+        self.liste_adherents = {}
         self.liste_emprunts = []
+        self.FICHIER_DOCUMENTS = 'Documents.csv'
+        self.FICHIER_ADHERENTS = 'Adherents.csv'
+        self.FICHIER_EMPRUNTS = 'Emprunts.csv'
