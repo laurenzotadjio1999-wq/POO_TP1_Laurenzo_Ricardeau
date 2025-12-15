a9562619187f1006439c86c7aca15a4b29449293
durandrica-hue <durandrica@gmail.com>
2025-12-14 21:01:38 -0500

    classe attente confir

---
+++ attente_confirmation.py
@@ -0,0 +1,3 @@
+def attendre_confirmation():
+    #Met le programme en pause jusqu'à ce que l'utilisateur appuie sur Entrée.
+    input("\nAppuyez sur ENTRÉE pour continuer et revenir au menu principal...")
